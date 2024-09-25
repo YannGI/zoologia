b60ca2907622b51e961cf1e10812080adbb69180
YannGI <yannou.girardo@gmail.com>
2024-09-25 12:26:38 +0200

    1st commit structure and config files : ms : configs/eurekas/webapps

--- z-configserver/src/main/java/YannGI/example/z_configserver/ZConfigserverApplication.java
+++ z-configserver/src/main/java/YannGI/example/z_configserver/ZConfigserverApplication.java
@@ -2,8 +2,10 @@ package YannGI.example.z_configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ZConfigserverApplication {
 
 	public static void main(String[] args) {
--- z-eurekaserver/src/main/java/YannGI/example/z_eurekaserver/ZEurekaserverApplication.java
+++ z-eurekaserver/src/main/java/YannGI/example/z_eurekaserver/ZEurekaserverApplication.java
@@ -2,8 +2,10 @@ package YannGI.example.z_eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ZEurekaserverApplication {
 
 	public static void main(String[] args) {
